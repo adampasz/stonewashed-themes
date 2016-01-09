8aff7e8eb34119cb840132ac19ed1ddd3d29b96c
adampasz <adampasz@gmail.com>
2016-01-09 10:35:43 -0800

    chromeos terminal settings

---
+++ chromeos-stonewashed-term.js
@@ -0,0 +1,9 @@
+term_.prefs_.resetAll();
+
+term_.prefs_.set('foreground-color', '#000000');
+term_.prefs_.set('background-color', '#EEEEEE');
+term_.prefs_.set('ctrl-v-paste', true);
+term_.prefs_.set('ctrl-c-copy', true);
+term_.prefs_.set('user-css', 'http://fonts.googleapis.com/css?family=Source+Code+Pro');
+term_.prefs_.set('font-family', '"Source Code Pro", monospace');
+term_.prefs_.set('font-size', 13);
